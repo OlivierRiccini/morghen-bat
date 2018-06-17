98b832ecefe57bbc86e37d5d4b21455be6d01ca7
Olivier Riccini <olivier-webdev@hotmail.com>
2018-06-17 11:54:28 -0400

    created home page sections

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('Hello World from Webpacker');
 
-$('#company-sub-categories').click(function() {
-  console.log('coucou!');
+$('#company-item, #company-sub-categories').hover(function() {
+  $('#company-sub-categories').slideToggle();
+});
+
+$('.nav-item').hover(function() {
+  $(this).toggleClass('nav-item-active');
 });
