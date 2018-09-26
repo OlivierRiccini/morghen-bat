c194a6598eeeef723c6230a8f1fafdbea7ba6880
Olivier Riccini <olivier-webdev@hotmail.com>
2018-09-26 16:12:31 +0200

    trying to debug in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ config.webpacker.check_yarn_integrity = false
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
