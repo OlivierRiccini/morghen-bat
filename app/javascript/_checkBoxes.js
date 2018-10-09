0bc3fb3dc2c0887adf650e49a991b8d7f5f67819
Olivier Riccini <olivier-webdev@hotmail.com>
2018-10-09 12:41:52 -0400

    form done

--- app/javascript/_checkBoxes.js
+++ app/javascript/_checkBoxes.js
@@ -4,13 +4,14 @@ const activateCheckBox = (event) => {
   const btn = event.target.parentElement;
   const input = btn.querySelector('input');
 
-  if (btn.classList.contains('check-box-cliked')) {
-    btn.classList.remove('check-box-cliked');
-    input.checked = false;
-  } else {
+  if (!input.checked && !btn.classList.contains('metier-checkboxes')) {
     btn.classList.add('check-box-cliked');
     input.checked = true;
+  } else {
+    btn.classList.remove('check-box-cliked');
+    input.checked = false;
   }
 }
 
 allCheckBoxBtns.forEach( e => e.addEventListener('click', activateCheckBox) );
+
